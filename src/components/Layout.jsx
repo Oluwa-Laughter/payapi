1b921133e25d8bc9cc9b6604778d2c126995db34
Oluwa-Laughter <210806073@live.unilag.edu.ng>
2025-01-08 12:37:39 +0100

    overflow fixed on Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,8 +5,9 @@ import Cta from "./Cta";
 
 function Layout() {
   return (
-    <div>
+    <div className="relative overflow-hidden ">
       <NavBar />
+      <div className="bg-circle -z-50 -left-[202px] -top-[550px] md:left-2/3 md:-top-[172px] overflow-x-hidden " />
       <main>
         <Outlet />
         <Cta />
